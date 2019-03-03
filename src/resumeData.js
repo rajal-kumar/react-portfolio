a06f645d31d827a866b7458624eb6c7f3b0366f8
Rajal Kumar <rajalkumar13@hotmail.com>
2019-03-03 23:24:09 +1300

    small changes

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,30 +1,30 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
+    "imagebaseurl":"https://www.rajalkumar.com",
+    "name": "Rajal Kumar",
+    "role": "Frontend/FullStack Developer and U/X Interface ",
+    "linkedinId": "rajal-kumar-3b00ab10b",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "HTML / CSS | JavaScript | React | React360 / VR | Redux | NodeJS | Express | SQL / liteDB | BootStrap",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/rajal-kumar-3b00ab10b/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/rajal-kumar",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
           "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
+          "className":"fa fa-skype"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme": "I like dabbling in various parts of web development and like to learn about new technologies, write technical articles or simply play games in my free time. I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Wellington, New Zealand",
+    "website":"https://www.rajalkumar.com",
     "education":[
       {
         "UniversityName":"The LNM Insitute of Information Technology",
@@ -73,22 +73,22 @@ let resumeData = {
       {
         "name":"project1",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"images/portfolio/origami.jpg"
       },
       {
         "name":"project2",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "imgurl":"images/portfolio/console.jpg"
       },
       {
         "name":"project3",
         "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "imgurl":"images/portfolio/coffee.jpg"
       },
       {
         "name":"project4",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"images/portfolio/judah.jpg"
       }
     ],
     "testimonials":[
