406881752b4a36665abc8646f298f790c5af73ef
Rajal Kumar <rajalkumar13@hotmail.com>
2019-03-28 12:41:27 +1300

    adding packages required for headless CMS setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
