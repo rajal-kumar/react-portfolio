c82c4634f326f31445a9a7c308e3882d48d56c08
Rajal Kumar <rajalkumar13@hotmail.com>
2019-04-01 11:52:11 +1300

    back at this after some hosting issues focusing on intergrating a cms for this or maybe even building one

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 export default class Porfolio extends Component {
   constructor(props) {
@@ -17,7 +18,7 @@ export default class Porfolio extends Component {
   }
 
   render() {
-    let resumeData = this.props.resumeData;
+    // let resumeData = this.props.resumeData;
     return (
       <section id="portfolio">
       <div className="row">
@@ -25,12 +26,19 @@ export default class Porfolio extends Component {
           <h1>Check Out Some of My Work.</h1>
           <p style={{textAlign: "center"}}>I apopogise about this section, I will ensure to get this section up and ready ASAP</p>
           <div>
-            {this.state.projects.map(projects => {
-              <Link to={`/${projects.slug}`} key={projects.id}
-            })}
+            {this.state.projects.map(projects => (
+              <Link to={`/${projects.slug}`} key={projects.id}>
+                <div className="card" key={projects.id}>
+                  <div className="cardContent">
+                    <h3>{projects.title.rendered}</h3>
+                      { projects.excerpt.rendered }
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
           
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {/* <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           {
             resumeData.portfolio && resumeData.portfolio.map((item, i)=>{
               return(
@@ -50,7 +58,7 @@ export default class Porfolio extends Component {
               )
             })
           }
-          </div>
+          </div> */}
         </div>
       </div>
   </section>
