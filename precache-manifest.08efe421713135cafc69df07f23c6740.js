bc90909b3acc2ae50345ff38964c51701e6579e4
Rajal Kumar <rajalkumar@protonmail.com>
2021-04-12 15:20:03 +1200

    Updates

---
+++ precache-manifest.08efe421713135cafc69df07f23c6740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf5451c468694d38852",
+    "url": "/react-portfolio/static/css/main.7cfa3632.chunk.css"
+  },
+  {
+    "revision": "adf5451c468694d38852",
+    "url": "/react-portfolio/static/js/main.adf5451c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "513a4df27cce5aeb0b4e37408820d8a1",
+    "url": "/react-portfolio/index.html"
+  }
+];
